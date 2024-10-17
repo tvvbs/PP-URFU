a3e21f7fa9654616959bdc66fc8073cc1c4220f3
tvvbs <alexandr-01@internet.ru>
2024-10-17 22:39:07 +0500

    initial commit

---
+++ WebApplication1/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers;
+
+public class UserController : ControllerBase
+{
+    IResult Register(string? login, 
+                     string? password,
+                     string? passwordRepeat, 
+                     string? name,
+                     string? surname)
+    {
+        
+    }
+
+    IResult Login(string? login, string? password)
+    {
+        
+    }
+}
