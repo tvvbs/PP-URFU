0eca406011c8136730df3fdd388921a19ab16aa5
tvvbs <alexandr-01@internet.ru>
2024-10-19 02:35:18 +0500

    api improve

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa2d20ada28c6a90a0afd0537b58abbcb7f41add")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+840c61112eb1fbdd31ab82edf1c1cadb44f97553")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
