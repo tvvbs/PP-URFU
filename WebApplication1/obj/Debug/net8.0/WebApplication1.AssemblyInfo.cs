5dae7a20617efbf012fdd0e13a99afc5ecedb487
tvvbs <alexandr-01@internet.ru>
2024-10-18 22:30:36 +0500

    setup cors one more tiem

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37a120fe7a9cfa7ced9f2fa11f5f89bee62ac617")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+141b8b0dacd70b1bbff5697b1d3405d8bea94699")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
