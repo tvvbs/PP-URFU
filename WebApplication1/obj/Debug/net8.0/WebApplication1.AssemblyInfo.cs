25e16c822d42e51f509793d9723fe82c8da4ac37
tvvbs <alexandr-01@internet.ru>
2024-10-19 21:38:52 +0500

    remove bin obj

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4635f5dade9a44dad7becb04fa46d9870bd14bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b3a422ffbc07b64ea63c0c41c81e37bd2188555")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
