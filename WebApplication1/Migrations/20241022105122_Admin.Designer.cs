caddffad19c8fd037e6cc24fe1f8fac6e96d6a17
tvvbs <alexandr-01@internet.ru>
2024-10-22 15:52:43 +0500

    mcgrations

--- WebApplication1/Migrations/20241022105122_Admin.Designer.cs
+++ WebApplication1/Migrations/20241022105122_Admin.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using WebApplication1.Database;
@@ -11,9 +12,11 @@ using WebApplication1.Database;
 namespace WebApplication1.Migrations
 {
     [DbContext(typeof(PracticeDbContext))]
-    partial class PracticeDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241022105122_Admin")]
+    partial class Admin
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
