994947a2161c3cbbb77d011be87acfca563dadb2
tvvbs <alexandr-01@internet.ru>
2024-10-24 01:38:35 +0500

    uidde

--- WebApplication1/Middleware/DbContextTransactionMiddleware.cs
+++ WebApplication1/Middleware/DbContextTransactionMiddleware.cs
@@ -15,11 +15,14 @@ public class DbContextTransactionMiddleware
     public async Task InvokeAsync(HttpContext context, PracticeDbContext dbContext, ILogger<DbContextTransactionMiddleware> logger)
     {
 
-        if (context.Request.Path.Value != null && context.Request.Path.Value.Contains("VacancyResponse/get-all"))
+        if (context.Request.Path.Value != null && context.Request.Path.Value.Contains("VacancyResponses/get-all"))
         {
+            Console.WriteLine("oiu");
             await _next(context);
             return;
         }
+
+        Console.WriteLine("oeuihtn");
         // create middleware for transactions
         await using var transaction = await dbContext.Database.BeginTransactionAsync();
         
